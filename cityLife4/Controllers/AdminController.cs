3d58bab5d1dec3bbe2f6b2b068a53550e9e89abc
atzmon ghilai <atzmon.ghilai@gmail.com>
2018-10-09 00:07:59 +0300

    city life DB and admin controller

---
+++ cityLife4/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cityLife4.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string recreateDB)
+        {
+            var a = recreateDB;
+           
+            FileStream sqlFile = new FileStream(Server.MapPath("/cityLifeDB.csv"),FileMode.Open);
+            bool b = sqlFile.CanRead;
+
+            return View();
+        }
+    }
+}
