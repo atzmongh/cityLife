f9ab74ceca2b8b7dd97738e396e08c1ed530943e
Atzmon Ghilai <atzmon.ghilai@gmail.com>
2019-02-01 23:19:32 +0200

    add update order

---
+++ cityLife4/classes/Version.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cityLife4
+{
+    public static class AppVersion
+    {
+        public static string current = "V1.0.3";
+    }
+}
