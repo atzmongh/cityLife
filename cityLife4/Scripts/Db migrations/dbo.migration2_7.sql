ee258e3d5fab4d9e2fc982e84cf0983d2b33b6c7
Atzmon Ghilai <atzmon.ghilai@gmail.com>
2021-07-22 21:49:04 +0300

    actual and expected revenue

---
+++ cityLife4/Scripts/Db migrations/dbo.migration2_7.sql
@@ -0,0 +1,20 @@
+﻿create table Services (
+Id int identity(1,1) not null,
+name varchar(255) not null,
+price decimal(6,2) not null,
+constraint PK_Services primary key(Id)
+);
+
+create table Ordered_services (
+Id int identity(1,1) not null,
+order_date datetime,
+amount int not null,
+unit_price decimal (6,2) not null,
+total_price decimal (6,2),
+done bit not null,
+order_id int not null,
+service_id int not null,
+constraint PK_Ordered_services primary key (Id),
+constraint FK_Ordered_servies_orders foreign key (order_id) references Orders(Id),
+constraint FK_Ordered_services_services foreign key (service_id) references Services(Id)
+);
